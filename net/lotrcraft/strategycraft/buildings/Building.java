b260d17394a97261a9dde1bd7705d4b6ea45bb2e
Francesco Macagno <fjmacagno1@yahoo.com>
2011-10-03 21:36:15 -0700

    Still working on schematics

--- net/lotrcraft/strategycraft/buildings/Building.java
+++ net/lotrcraft/strategycraft/buildings/Building.java
@@ -5,12 +5,17 @@ import org.bukkit.Location;
 public abstract class Building {
 	
 	void build(Location l){
-		int x, y, z;
 		Schematic s = getSchematic();
 		byte[] bytes = s.getBlocks();
 		
+		Location corner = new Location(l.getWorld(), s.getWidth()/2, l.getBlockY(), s.getLength()/2);
+		
 		for (int y = 0; y < s.getHeight(); y++){
-			l.getWorld().getBlockAt(bytes.)
+			for (int x = 0; x < s.getWidth(); x++){
+				for (int z = 0; z < s.getLength(); z++){
+					l.getWorld().getBlockAt(corner).setData(bytes[y][x][z]);
+				}
+			}
 		}
 		
 	}
