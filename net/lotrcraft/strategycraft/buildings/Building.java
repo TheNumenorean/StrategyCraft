44ab3f699f4da4e0ed30d462bd402a9f64fad2ee
Francesco Macagno <fjmacagno1@yahoo.com>
2011-10-04 20:11:25 -0700

    Working on schematic reading

--- net/lotrcraft/strategycraft/buildings/Building.java
+++ net/lotrcraft/strategycraft/buildings/Building.java
@@ -4,19 +4,27 @@ import org.bukkit.Location;
 
 public abstract class Building {
 	
-	void build(Location l){
+	private static BuildingManager bm;
+	
+	Location location;
+	
+	public void build(Location l){
 		Schematic s = getSchematic();
 		byte[] bytes = s.getBlocks();
 		
-		Location corner = new Location(l.getWorld(), s.getWidth()/2, l.getBlockY(), s.getLength()/2);
+		location = new Location(l.getWorld(), s.getWidth()/2, l.getBlockY(), s.getLength()/2);
 		
 		for (int y = 0; y < s.getHeight(); y++){
 			for (int x = 0; x < s.getWidth(); x++){
 				for (int z = 0; z < s.getLength(); z++){
-					l.getWorld().getBlockAt(corner).setData(bytes[y + x + z]);
+					l.getWorld().getBlockAt(location).setData(bytes[y + x + z]);
 				}
 			}
 		}
+		
+	}
+	
+	public void destroy(){
 		
 	}
 	
@@ -28,7 +36,7 @@ public abstract class Building {
 	
 	//TODO: does this work?
 	BuildingManager getBuildingManager(){
-		return new BuildingManager();
+		return bm;
 	}
 
 
