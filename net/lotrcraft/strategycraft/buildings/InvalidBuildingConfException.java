8ef717ab01a33ee7094266177626ca4ff54d98b1
Francesco Macagno <fjmacagno1@yahoo.com>
2011-10-19 21:39:18 -0700

    improved the exception.

--- net/lotrcraft/strategycraft/buildings/InvalidBuildingConfException.java
+++ net/lotrcraft/strategycraft/buildings/InvalidBuildingConfException.java
@@ -1,8 +1,13 @@
 package net.lotrcraft.strategycraft.buildings;
 
 public class InvalidBuildingConfException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3304187251577762755L;
+
 	public InvalidBuildingConfException(){
-		super();
+		super("Invalid configuration file!");
 	}
 
 }
