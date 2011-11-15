6101da621e82cc9e2a9893376a6b98e513445a14
Francesco Macagno <fjmacagno1@yahoo.com>
2011-11-14 18:23:12 -0800

    Small changes

--- net/lotrcraft/strategycraft/units/UnitManager.java
+++ net/lotrcraft/strategycraft/units/UnitManager.java
@@ -1,5 +1,9 @@
 package net.lotrcraft.strategycraft.units;
 
 public class UnitManager {
+	public Entity[] getUnitsAtLoc(Location loc){
+		return null;
+	}
 	
+	public Entity[] getUnitsAtLoc(Location loc, int )
 }
