b0684992a6d69f8267d936e5f650764d29749f45
Francesco Macagno <fjmacagno1@yahoo.com>
2012-01-31 21:00:02 -0800

    Finished adding comments to Config.java

--- net/lotrcraft/strategycraft/Config.java
+++ net/lotrcraft/strategycraft/Config.java
@@ -1,3 +1,10 @@
+/**
+ * This file will undergo massive changes once i get around to updateing it for the newer Configuration versions,
+ *  and once it is updated for MySql.
+ *  
+ *  TODO: Update to FileConfiguration
+ *  TODO: Change to MySql
+ */
 package net.lotrcraft.strategycraft;
 
 import java.io.File;
@@ -16,7 +23,6 @@ import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.util.config.Configuration;
 
-@SuppressWarnings("deprecation")
 public class Config {
 	
 	public static int coreBlock;
@@ -95,7 +101,7 @@ public class Config {
 		StrategyCraft.log.info("[StrategyCraft] Loading Castles...");
 		
 		//Load Player files
-		//TODO: replace with MySql!
+		//TODO: replace with MySql! This section is very ineffective, and will not load all buildings.
 		
 		//Check to make sure playerDataFolder exists
 		if (!playerDataFolder.exists()){
@@ -118,22 +124,28 @@ public class Config {
 				playerConfig = new Configuration(playerFiles[counter]);
 				playerConfig.load();
 				
+				//Get Citadel x coordinate
 				if (isNull("Castle.Citadel.X", playerConfig)){
 					StrategyCraft.log.info("[StrategyCraft] Config for " + playerName + " missing X for citadel. Rejecting...");
 					continue;
 				}
 				int x = playerConfig.getInt("Castle.Citadel.X", -1);
+				
+				//Get Citadel y coordinate
 				if (isNull("Castle.Citadel.Y", playerConfig)){
 					StrategyCraft.log.info("[StrategyCraft] Config for " + playerName + " missing Y for citadel. Rejecting...");
 					continue;
 				}
 				int y = playerConfig.getInt("Castle.Citadel.Y", -1);
+				
+				//Get Citadel z coordinate
 				if (isNull("Castle.Citadel.Z", playerConfig)){
 					StrategyCraft.log.info("[StrategyCraft] Config for " + playerName + " missing Z for citadel. Rejecting...");
 					continue;
 				}
 				int z = playerConfig.getInt("Castle.Citadel.Z", -1);
 				
+				//Get citadel World
 				if (isNull("Castle.Citadel.World", playerConfig)){
 					StrategyCraft.log.info("[StrategyCraft] Config for " + playerName + " missing World for citadel. Rejecting...");
 					continue;
@@ -145,8 +157,10 @@ public class Config {
 					continue;
 				}
 				
+				//Get new location from acquired data 
 				Location location = new Location(world, x, y, z);
 				
+				//Add new castle into list
 				castle = new Castle(location, playerName);
 				BuildingManager.addCastle(playerName, castle);
 				
@@ -157,10 +171,19 @@ public class Config {
 			}
 		}
 		
+		//This definitely doesnt send a string to the logger
 		StrategyCraft.log.info("[StrategyCraft] Finished loading castles.");
 		
 		StrategyCraft.config.save();
 	}
+	
+	/**
+	 * Get a property from a given location. If the location is null, it will be set to the value of def.
+	 * @param path Path to node
+	 * @param def Default value
+	 * @param config Config to read
+	 * @return The recieved object, def if none is found.
+	 */
 
 	public Object getProperty(String path, Object def, Configuration config) {
 		if(isNull(path, config))
@@ -168,27 +191,58 @@ public class Config {
 		return config.getProperty(path);
 	}
 
+	/**
+	 * Get an int at a given location. If the location is null, it will be set to the value of def.
+	 * @param path Path to node
+	 * @param def Default value
+	 * @param config Config to read
+	 * @return Recieved value, or def if none is found
+	 */
 	public static int getInt(String path, Integer def, Configuration config) {
 		if(isNull(path, config))
 			return (Integer) setProperty(path, def, config);
 		return config.getInt(path, def);
 	}
 
-	public static Boolean getBoolean(String path, Boolean def, Configuration config) {
+	/**
+	 * Gets a boolean at a given location. If the location points to a null object as defined by isNull , will set the node to the given value.
+	 * @param path Path to node
+	 * @param def Default value
+	 * @param config Config to use
+	 * @return The value, or def if the link is null.
+	 */
+	public static boolean getBoolean(String path, Boolean def, Configuration config) {
 		if(isNull(path, config))
 			return (Boolean) setProperty(path, def, config);
 		return config.getBoolean(path, def);
 	}
 
+	/**
+	 * Sets the defined path to the object's value
+	 * @param path Path to node
+	 * @param val Value to use
+	 * @param config Config to use
+	 * @return The given object
+	 */
 	private static Object setProperty(String path, Object val, Configuration config) {
 		config.setProperty(path, val);
 		return val;
 	}
 
+	/**
+	 * Checks to see whether a specified path points to a null object
+	 * @param path Path to configuration node
+	 * @param config The configuration to check
+	 * @return Whether the specified path points to an existant node 
+	 */
 	private static boolean isNull(String path, Configuration config) {
 		return config.getProperty(path) == null;
 	}
 
+	/**
+	 * Deletes a configuration file for a certain Player
+	 * @param owner The name of the player whose conf is to be deleted.
+	 */
 	public static void removePlayerConf(String owner) {
 		File file = new File(File.pathSeparator + "StrategyCraft" + File.pathSeparator + "PlayerCastles" + File.pathSeparator + owner + ".yml");
 		if (file.exists()){
